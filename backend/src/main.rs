304e351972aaf4c782396bc3d6c9474a4537456c
hassiy <hassiy@disroot.org>
2024-05-22 11:55:07 +0300

    Add the middleware to Actix Web server setup

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, App, HttpServer, HttpResponse};
 use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
+use actix_web_httpauth::middleware::HttpAuthentication;
 
 #[macro_use]
 extern crate diesel;
@@ -49,9 +50,13 @@ async fn create_post(post: web::Json<CreatePost>) -> HttpResponse {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .wrap(HttpAuthentication::bearer(jwt_middleware))
             .route("/register", web::post().to(register_user))
             .route("/login", web::post().to(login_user))
             .route("/posts", web::post().to(create_post))
+            .route("/posts", web::get().to(get_posts))
+            .route("/posts/{id}", web::put().to(update_post))
+            .route("/posts/{id}", web::delete().to(delete_post))
     })
     .bind("127.0.0.1:8080")?
     .run()
